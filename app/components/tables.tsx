ebb2103b16ea53f3ffb61a8437363a7a81a7c372
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-12-01 15:35:11 +0900

    feat: add four peoples table

--- app/components/tables.tsx
+++ app/components/tables.tsx
@@ -1,23 +1,68 @@
+import { getEmoji } from 'app/utils/myFunction';
+
 export default function Tables() {
   const createTable = (start: number, end: number, has2ndDiv: boolean) => {
     return Array.from({ length: end - start + 1 }, (_, index) => (
-      <div key={index} className="w-10 m-5 flex flex-col">
+      <button key={index} className='m-4 flex flex-col items-center' onClick={() => handleClick(start + index)}>
         <p>{start + index}</p>
-        <div className="w-full h-10 border flex items-center justify-center">
-          <p className="text-xl"></p>
-        </div>
-        {has2ndDiv && (
-          <div className="w-full h-10 border flex items-center justify-center">
-            <p className="text-xl"></p>
-          </div>
-        )}
-        <div className="w-full h-14 border">
-          <p className="text-xs text-center"></p>
-        </div>
-      </div>
+        {
+          has2ndDiv ?
+            <>
+              {
+                index !== end - start ?
+                  <div>
+                    <div className='w-10 h-10 border items-center justify-center'>
+                      <p className='text-xl'>{getEmoji(10, 'F')}</p>
+                    </div>
+                    <div className='w-10 h-10 border items-center justify-center'>
+                      <p className='text-xl'>{getEmoji(10, 'F')}</p>
+                    </div>
+                    <div className="w-full h-14 border">
+                      <p className="text-xs text-center"></p>
+                    </div>
+                  </div>
+                  :
+                  <div className=''>
+                    <div className='flex'>
+                      <div className='w-10 h-10 border items-center justify-center'>
+                        <p className='text-xl'>{getEmoji(10, 'F')}</p>
+                      </div>
+                      <div className='w-10 h-10 border items-center justify-center'>
+                        <p className='text-xl'>{getEmoji(10, 'F')}</p>
+                      </div>
+                    </div>
+                    <div className='flex'>
+                      <div className='w-10 h-10 border items-center justify-center'>
+                        <p className='text-xl'>{getEmoji(10, 'F')}</p>
+                      </div>
+                      <div className='w-10 h-10 border items-center justify-center'>
+                        <p className='text-xl'>{getEmoji(10, 'F')}</p>
+                      </div>
+                    </div>
+                    <div className="w-full h-14 border">
+                      <p className="text-xs text-center"></p>
+                    </div>
+                  </div>
+              }
+            </>
+            :
+            <>
+              <div className="w-10 h-10 border flex items-center justify-center">
+                <p className="text-xl">{getEmoji(10, 'F')}</p>
+              </div>
+              <div className="w-10 h-14 border">
+                <p className="text-xs text-center"></p>
+              </div>
+            </>
+        }
+      </button>
     ));
   };
 
+  const handleClick = (index: number) => {
+    console.log(index)
+    return ''
+  }
   return (
     <div>
       <div className="flex items-center">
