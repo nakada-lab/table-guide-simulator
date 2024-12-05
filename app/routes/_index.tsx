37b7a4b480457e2066c8b575e32622dc3e815b5f
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-12-05 16:16:06 +0900

    feat: Change UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,22 +13,24 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center">
       <Form method="post" action="/play">
         <div className="flex flex-col w-full max-w-xs">
-          <h1 className="my-5">1時間の営業を好きな時間に圧縮できます
-          </h1>
+          <h1 className="my-5">1時間の営業を好きな時間に圧縮できます</h1>
           <span className="label-text mb-2">何分でやる？</span>
           <div className="flex items-center">
-            <input
-              type="text"
-              placeholder="3"
-              name="min"
-              className="input input-bordered flex-grow"
-              required
-            />
-            <p className="ml-2">分</p>
+            <div className="flex-1">
+              <input
+                type="number"
+                name="numericField"
+                min="0"
+                step="1"
+                placeholder="2"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <span className="ml-2">分</span>
           </div>
         </div>
-        <button type="submit" className="btn mt-4">Play</button>
+        <button type="submit" className="btn my-4 w-full">Play</button>
       </Form>
-    </div >
+    </div>
   );
 }
