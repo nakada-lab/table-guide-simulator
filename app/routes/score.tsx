c08d082408793b47f1a61599da6998e791fae44c
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-12-13 00:21:24 +0900

    feat: Implement score page

--- app/routes/score.tsx
+++ app/routes/score.tsx
@@ -1,15 +1,63 @@
 import { useLocation } from '@remix-run/react';
+import { useEffect, useState } from 'react';
+import { supabase } from '~/utils/supabase';
 
 export default function Score() {
+  const [score, setScore] = useState([]);
   const location = useLocation();
-  const { name, year, score, uuid } = location.state;
+
+  let selectedUuid = '';
+  if (location.state) {
+    const { uuid } = location.state;
+    selectedUuid = uuid;
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        let { data: score, error } = await supabase
+          .from('score')
+          .select('*');
+
+        if (error) {
+          console.error(error);
+          return;
+        }
+        setScore([...score].sort((a, b) => a.score - b.score));
+        //setScore([...data].sort((a, b) => b.score - a.score));
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
 
   return (
     <div>
-      <p>uuid:{uuid}</p>
-      <p>name:{name}</p>
-      <p>year:{year}</p>
-      <p>score:{score}</p>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead className="bg-slate-400">
+            <tr>
+              <th className="text-center">No.</th>
+              <th>名前</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {score.map((item, index) => (
+              <tr
+                key={item.uuid}
+                className={`${item.uuid === selectedUuid ? "bg-yellow-200" : "odd:bg-white even:bg-slate-100"
+                  }`}
+              >
+                <th className="text-center">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
-  )
+  );
 }
